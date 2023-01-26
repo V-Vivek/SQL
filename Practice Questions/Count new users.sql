ef65c367eed9c5aef3494c11224822f9b816d882
Vivek Jadhav <117569148+V-Vivek@users.noreply.github.com>
2023-01-26 21:28:06 +0530

    Create Count new users.sql

---
+++ Practice Questions/Count new users.sql
@@ -0,0 +1,30 @@
+/*
+Problem statement: Calculate unique new user count for each day
+Question link: https://youtu.be/HZtUxYdnbhw
+*/
+
+-- Dataset:
+
+CREATE TABLE user_act(tra_dt DATE, user_id INT, activity VARCHAR(20));
+INSERT INTO user_act VALUES ('2022-02-20', 1, 'abc');
+INSERT INTO user_act VALUES ('2022-02-20', 2, 'xyz');
+INSERT INTO user_act VALUES ('2022-02-22', 1, 'xyz');
+INSERT INTO user_act VALUES ('2022-02-22', 3, 'klm');
+INSERT INTO user_act VALUES ('2022-02-24', 1, 'abc');
+INSERT INTO user_act VALUES ('2022-02-24', 2, 'abc');
+INSERT INTO user_act VALUES ('2022-02-24', 3, 'abc');
+SELECT * FROM user_act;
+
++------------+---------+----------+
+| tra_dt     | user_id | activity |
++------------+---------+----------+
+| 2022-02-20 |       1 | abc      |
+| 2022-02-20 |       2 | xyz      |
+| 2022-02-22 |       1 | xyz      |
+| 2022-02-22 |       3 | klm      |
+| 2022-02-24 |       1 | abc      |
+| 2022-02-24 |       2 | abc      |
+| 2022-02-24 |       3 | abc      |
++------------+---------+----------+
+
+-- Solution:
