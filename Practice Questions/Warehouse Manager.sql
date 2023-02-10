6c84630757008966a646546beed9bea47aec8ba0
Vivek Jadhav <117569148+V-Vivek@users.noreply.github.com>
2023-02-10 22:41:41 +0530

    Update Warehouse Manager.sql

--- Practice Questions/Warehouse Manager.sql
+++ Practice Questions/Warehouse Manager.sql
@@ -1,1 +1,8 @@
+-- Question Link: https://www.codingninjas.com/codestudio/problems/warehouse-manager_2181185?utm_source=youtube&utm_medium=affiliate&utm_campaign=sqlproblemlist_shashank
 
+-- Solution
+
+SELECT name AS warehouse_name, SUM(units * Width * Length * Height) AS volume
+FROM Warehouse w JOIN Products p
+ON w.product_id = p.product_id
+GROUP BY 1;
